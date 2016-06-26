726524c1257a5f05ce08c127f6694d03ea77bb83
Michael Straube <m.s.online@gmx.de>
2016-06-26 15:41:10 +0200

    Remove unused windows code [WinAPI]

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,12 @@
 /// WIN32 INCLUDES
 #elif defined (WIN32)
 
-//#define INITGUID       // make sure all the COM interfaces are available
-// instead of this you can include the .LIB file
-// DXGUID.LIB
-
-//#define WIN32_LEAN_AND_MEAN
-
-//#include <windows.h>   // include important windows stuff
-//#include <windowsx.h>
-//#include <mmsystem.h>
-
-//#include <conio.h>
-//#include <io.h>
-
 #include <getopt.h> // MinGW
 
 #endif
 
-//#define INITGUID       // make sure all the COM interfaces are available
-// instead of this you can include the .LIB file
-// DXGUID.LIB
 
-//#define WIN32_LEAN_AND_MEAN
-
-//#include <windows.h>   // include important windows stuff
-//#include <windowsx.h>
-//#include <mmsystem.h>
 #include <iostream> // include important C/C++ stuff
-//#include <conio.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
@@ -45,17 +23,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <math.h>
-//#include <io.h>
 #include <fcntl.h>
 
 #include <SDL2/SDL.h>
-
 #include "MickSDLSound.h"
-
-// some more win32 defines
-//#if defined (WIN32)
-//#include <ddraw.h>  // directX includes
-//#endif
 
 // game object includes
 #include "GameEngine.h"
@@ -64,15 +35,9 @@
 #define NULL 0
 #endif
 
-// MACROS /////////////////////////////////////////////////
-
-#define KEYDOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
-#define KEYUP(vk_code)   ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
-
 // DEFINES ////////////////////////////////////////////////
 
 // defines for windows interface
-#define WINDOW_CLASS_NAME "Bit Riot"  // class name
 #define WINDOW_TITLE      "Bit Riot Beta - SDL2 Port"
 const int WINDOW_WIDTH = (Map::MAP_WIDTH * Map::TILE_WIDTH) + InfoPanel::WIDTH;   // size of window
 const int WINDOW_HEIGHT = Map::MAP_HEIGHT * Map::TILE_HEIGHT;
@@ -84,15 +49,15 @@ const int WINDOW_HEIGHT = Map::MAP_HEIGHT * Map::TILE_HEIGHT;
 // also if 8-bit the a pallete
 // is created and attached
 
-#define WINDOWED_APP      1     // 0 not windowed, 1 windowed
+// Ouestion: is the above comment still valid for SDL2 ?
 
 //const unsigned int COLORKEY_LOW = 0; // low and high values for source
 //const unsigned int COLORKEY_HIGH = 0; // color keying (transparency range)
 
-const Uint8 COLORKEY_R = 0;
-const Uint8 COLORKEY_G = 0;
-const Uint8 COLORKEY_B = 0;
-const Uint8 COLORKEY_A = 0;
+//const Uint8 COLORKEY_R = 0;
+//const Uint8 COLORKEY_G = 0;
+//const Uint8 COLORKEY_B = 0;
+//const Uint8 COLORKEY_A = 0;
 
 // Controls initialisation parameters, such as FULLSCREEN, etc
 unsigned int initFlags = 0; // start with all turned off
@@ -109,202 +74,16 @@ int consoleMain();
 
 // GLOBALS ////////////////////////////////////////////////
 
-//HWND main_window_handle           = NULL; // save the window handle
-//HINSTANCE main_instance           = NULL; // save the instance
-
 SDL_Window* sdl_window = NULL;
 SDL_Renderer* sdl_renderer = NULL;
 SDL_Texture* sdl_primary_texture = NULL;
 SDL_Surface* sdl_primary = NULL;
 
 // game object globals
-GameEngine * engine = NULL;
-
+GameEngine* engine = NULL;
 
 
 // FUNCTIONS //////////////////////////////////////////////
-/*
-LRESULT CALLBACK WindowProc(HWND hwnd,
-                UINT msg,
-                            WPARAM wparam,
-                            LPARAM lparam)
-{
-  // this is the main message handler of the system
-  PAINTSTRUCT ps;      // used in WM_PAINT
-  HDC     hdc;     // handle to a device context
-
-  // what is the message
-  switch(msg)
-    {
-    case WM_CREATE:
-      {
-      // do initialization stuff here
-      return(0);
-      } break;
-
-    case WM_PAINT:
-      {
-      // start painting
-      hdc = BeginPaint(hwnd,&ps);
-
-      // end painting
-      EndPaint(hwnd,&ps);
-      return(0);
-      } break;
-
-    case WM_MOVE:
-      {
-        int clientX, clientY;
-        // get new window co-ords
-        clientX = LOWORD(lparam);
-        clientY = HIWORD(lparam);
-
-        // update blitting RECT
-        clientArea.x = clientX;
-        clientArea.y = clientY;
-        clientArea.w = clientX + WINDOW_WIDTH -1 ;
-        clientArea.h = clientY + WINDOW_HEIGHT - 1;
-        return 0;
-      } break;
-
-
-    case WM_DESTROY:
-      {
-      // kill the application
-      PostQuitMessage(0);
-      return(0);
-      } break;
-
-    default:
-      break;
-
-    } // end switch
-
-  // process any messages that we didn't take care of
-  return (DefWindowProc(hwnd, msg, wparam, lparam));
-
-} // end WinProc
-
-// WINMAIN ////////////////////////////////////////////////
-/*
-int WINAPI WinMain( HINSTANCE hinstance,
-          HINSTANCE hprevinstance,
-          LPSTR lpcmdline,
-          int ncmdshow)
-{
-  // this is the winmain function
-  WNDCLASSEX winclass; // this will hold the class we create
-  HWND     hwnd;   // generic window handle
-  MSG      msg;    // generic message
-  HDC        hdc = NULL;      // graphics device context
-
-  // first fill in the window class stucture
-  winclass.cbSize         = sizeof(WNDCLASSEX);
-  winclass.style      = CS_DBLCLKS | CS_OWNDC |
-              CS_HREDRAW | CS_VREDRAW;
-  winclass.lpfnWndProc  = WindowProc;
-  winclass.cbClsExtra   = 0;
-  winclass.cbWndExtra   = 0;
-  winclass.hInstance    = hinstance;
-  winclass.hIcon      = LoadIcon(NULL, IDI_APPLICATION);
-  winclass.hCursor    = LoadCursor(NULL, IDC_ARROW);
-  winclass.hbrBackground  = (HBRUSH)GetStockObject(BLACK_BRUSH);
-  winclass.lpszMenuName = NULL;
-  winclass.lpszClassName  = WINDOW_CLASS_NAME;
-  winclass.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
-
-  // register the window class
-  if (!RegisterClassEx(&winclass))
-    return(0);
-
-  // create the window
-  if (!(hwnd = CreateWindowEx(NULL,              // extended style
-                WINDOW_CLASS_NAME, // class
-                WINDOW_TITLE, // title
-                (WINDOWED_APP ? (WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION) : (WS_POPUP | WS_VISIBLE)),
-                0,0,    // initial x,y
-                WINDOW_WIDTH,WINDOW_HEIGHT,  // initial width, height
-                NULL,   // handle to parent
-                NULL,   // handle to menu
-                hinstance,// instance of this application
-                NULL)))   // extra creation parms
-  return(0);
-
-  // save the window handle and instance in a global
-  main_window_handle = hwnd;
-  main_instance      = hinstance;
-
-  // resize the window so that client is really width x height
-  if (WINDOWED_APP)
-  {
-    // now resize the window, so the client area is the actual size requested
-    // since there may be borders and controls if this is going to be a windowed app
-    // if the app is not windowed then it won't matter
-    windowRect.top = 0;
-    windowRect.left = 0;
-    windowRect.right = WINDOW_WIDTH-1;
-    windowRect.bottom = WINDOW_HEIGHT-1;
-
-
-    // make the call to adjust window_rect
-    AdjustWindowRectEx(&windowRect,
-    GetWindowStyle(main_window_handle),
-    GetMenu(main_window_handle) != NULL,
-    GetWindowExStyle(main_window_handle));
-
-    // now resize the window with a call to MoveWindow()
-    MoveWindow(main_window_handle,
-        ((GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH)/2), // x position
-        ((GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT)/2), // y position
-        windowRect.right - windowRect.left, // width
-        windowRect.bottom - windowRect.top, // height
-        FALSE);
-
-    // show the window, so there's no garbage on first render
-    ShowWindow(main_window_handle, SW_SHOW);
-  } // end if windowed
-
-  // perform all console specific initialization
-  consoleInit();
-
-  // enter main event loop
-  while(1)
-    {
-    if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
-    {
-      // test if this is a quit
-      if (msg.message == WM_QUIT)
-      break;
-
-      // translate any accelerator keys
-      TranslateMessage(&msg);
-
-      // send the message to the window proc
-      DispatchMessage(&msg);
-    } // end if
-
-    // main game processing goes here
-    consoleMain();
-
-    // check if user is trying to exit
-    if (KEYDOWN(VK_ESCAPE))
-    {
-      PostMessage(main_window_handle, WM_DESTROY,0,0);
-    }
-
-    } // end while
-
-  // shutdown game and release all resources
-  consoleShutdown();
-
-
-  // return to Windows like this
-  return(int)(msg.wParam);
-
-} // end WinMain
-*/
-
-
 
 int consoleInit()
 {
@@ -381,12 +160,8 @@ int consoleInit()
   clientArea.h = WINDOW_HEIGHT - 1;
 
   SDL_SetClipRect(sdl_primary, &clientArea);
-  // set colour keying here */
-  //DDCOLORKEY colorKey;
-  //colorKey.dwColorSpaceLowValue = COLORKEY_LOW;
-  //colorKey.dwColorSpaceHighValue = COLORKEY_HIGH;
-  //lpddsback->SetColorKey(DDCKEY_SRCBLT, &colorKey);
 
+  // set colour keying here */
   //SDL_SetColorKey(sdl_back, SDL_SRCCOLORKEY, SDL_MapRGB(sdl_back->format, COLORKEY_R, COLORKEY_G, COLORKEY_B));
 
   // seed random number generator
